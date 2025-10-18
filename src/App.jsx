cc65a23c4d2b05362b849bca849a2a0f54547aac
AndFkx <134234718+AndFkx@users.noreply.github.com>
2025-10-18 08:33:04 +0000

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
+
+import React, { useState } from 'react';
+
+import Detalle from './componentes/Detalle';
+import Favoritos from './componentes/Favoritos';
+import Home from './componentes/Home';
+import Informativa from './componentes/Informativa';
+import Mapa from './componentes/Mapa';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <Router>
+
+          <nav className="c-menu">
+            <Link to="/">Home</Link>
+            <Link to="/informativa">Informativa</Link>
+            <Link to="/mapa">Mapa</Link>
+            <Link to="/favoritos">Favoritos</Link>
+          </nav>
+
+
+        <Routes>
+            <Route path="/" element={<Home /> } />
+            <Route path="/informativa" element={<Informativa /> } />
+            <Route path="/mapa" element={<Mapa /> } />
+            <Route path="/favoritos" element={<Favoritos /> } />
+            <Route path="/detalle/:depto/:municipio" element={<Detalle /> } />
+        </Routes>
+      </Router>
   )
 }
 
